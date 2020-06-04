03f10fef3eebc9b9ad0222f94d210ff5a718d8da
John Saputo <jsaputo1@gmail.com>
2020-06-04 21:50:14 +0000

    added code for fly over times

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -13,5 +13,12 @@ fetchMyIP((error, ip) => {
     console.log(
       `It worked! Returned Latitude: ${coordinates.latitude}, Longtitude: ${coordinates.longitude}`
     );
+    fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+      if (error) {
+        console.log("It didn't work!", error);
+        return;
+      }
+      console.log("It worked! Returned flyover times:", passTimes);
+    });
   });
 });
--- iss.js
+++ iss.js
@@ -47,4 +47,26 @@ const fetchCoordsByIP = function (ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function (coordinates, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+    const data = JSON.parse(body).response;
+    callback(null, data);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
